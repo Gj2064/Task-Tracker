7a61ad4425776fe593098637802736904f49132b
Gj2064 <jain.garima2003@gmail.com>
2025-03-04 17:47:41 +0530

    build check

--- src/main/java/com/example/tasktracker/config/CorsConfig.java
+++ src/main/java/com/example/tasktracker/config/CorsConfig.java
@@ -3,7 +3,7 @@ package com.example.tasktracker.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//checking
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
